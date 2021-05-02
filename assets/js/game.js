7f3c1c613131791454569a4f1470dedfbea705ac
Deb Orler <debbieorler@gmail.com>
2021-05-02 17:28:15 -0500

    operators

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,26 +1,57 @@
-function fight() {
-    window.alert("The fight has begun!");
+var playerName = window.prompt("What is your robot's name? ");
+var playerHealth = 100;
+var playerAttack = 10;
+
+//Note to remember to console log.
+console.log(playerName, playerAttack, playerHealth);
+
+
+var enemyName = "Roberto" ;
+var enemyHealth = 50 ;
+var enemyAttack = 12 ;
+
+// console
+console.log(enemyName, enemyHealth, enemyAttack);
+
+//Fight Function
+
+var fight = function() {
+    window.alert ("Welcome to Robot Gladiators!");
+// subtract the value of player Attack from the value of enemy health and use that result to update the value in the enemy health variable 
+    enemyHealth = enemyHealth - playerAttack;
+
+//log result message to console 
+console.log (
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
+);
+
+//check enemy health
+
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+//subtrat the value of enemyAttack from the value of playerHealth and use result to update playerHealth
+    playerHealth = playerHealth - enemyAttack ;
+
+//log to console
+console.log (
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
+);
+
+// check player's health
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
   }
 
-var playerName = window.prompt("What is your robots name?");
-// Note the lack of quotation marks around playerName 
-console.log ("this logs a string, good for leaving yourself a message");
-console.log ( 10 + 10 ); 
-console.log ("Our robots name is " + playerName);
+};
 
-var yourName = "Debbie";
+/* Bottom of page  to execute the function*/
 
-console.log(yourName);
-
-
-
-
-
-
-
-
-
-
-/* Bottom of page */
-
- // fight();
+ fight();
