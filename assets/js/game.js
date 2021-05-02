2536331242d40ed58f45cb2093581028a54ca68e
Deb Orler <debbieorler@gmail.com>
2021-05-02 15:15:14 -0500

    Java-Basics

---
+++ assets/js/game.js
@@ -0,0 +1,26 @@
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+var playerName = window.prompt("What is your robots name?");
+// Note the lack of quotation marks around playerName 
+console.log ("this logs a string, good for leaving yourself a message");
+console.log ( 10 + 10 ); 
+console.log ("Our robots name is " + playerName);
+
+var yourName = "Debbie";
+
+console.log(yourName);
+
+
+
+
+
+
+
+
+
+
+/* Bottom of page */
+
+ // fight();
