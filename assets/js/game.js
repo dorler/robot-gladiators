8a65fb2bcdaf7fc641e995beebfcad57f8a9e58b
Deb Orler <debbieorler@gmail.com>
2021-05-09 12:47:16 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,7 +28,7 @@ var fight = function(enemyName) {
         if (confirmSkip) {
          window.alert(playerName + " has decided to skip this fight. Goodbye!");
          // THEN subtract money from playerMoney for skipping
-         playerMoney = playerMoney - 10;
+         playerMoney = mathMax(0, playerMoney-10);
         console.log("playerMoney", playerMoney);
          break;
     }
@@ -36,10 +36,10 @@ var fight = function(enemyName) {
  
 //FIGHT
     // 1. remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
-        console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-        );
+        // generate random damage value based on player's attack power
+         var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
   
      // 1.a check enemy's health
              if (enemyHealth <= 0) {
@@ -53,7 +53,9 @@ var fight = function(enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left."); }
   
         // 2. remove player's health by subtracting the amount set in the enemyAttack variable
-         playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyAttack - 3, enemyAttack);
+            playerHealth = Math.max(0, playerHealth - damage);
+            
             console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
             );
@@ -81,7 +83,7 @@ var startGame = function () {
     
     var pickedEnemyName = enemyNames[i];
      
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
 
     fight(pickedEnemyName);
    
@@ -160,12 +162,18 @@ switch (shopOptionPrompt) {
     case "LEAVE":
       window.alert("Leaving the store.");
       break;
-      
+
     default:
       window.alert("You did not pick a valid option. Try again.");
       shop();
       break;
   }
+  };
+  // function to generate a random numeric value
+  var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
   };
 //call
 startGame ();
